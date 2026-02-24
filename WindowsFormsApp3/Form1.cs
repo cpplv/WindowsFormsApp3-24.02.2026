bf8dfff4476593182c77e05ae5c2a7c61701bbb8
Patric <pmorevs@gmail.com>
2026-02-24 11:05:22 +0200

    Name button

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -26,5 +26,10 @@ namespace WindowsFormsApp3
         {
             textBox1.Text = "Bye!";
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Andrejs";
+        }
     }
 }
